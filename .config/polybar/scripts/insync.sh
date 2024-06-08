13eef2044f4835a11ede33b2b554885ad43695cb
Brett Crisp <brettcrisp2@gmail.com>
2024-06-08 20:00:02 +0700

    Automated commit on 2024-06-08 20:00:01

--- .config/polybar/scripts/insync.sh
+++ .config/polybar/scripts/insync.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-# Path to the red and green icons
-RED_ICON="/home/brett/Pictures/insync-red.png"
-GREEN_ICON="/home/brett/Pictures/insync-green.png"
-
 # Check if InSync is running
 if pgrep -x "insync" > /dev/null; then
-    feh --bg-scale "$GREEN_ICON" # Set background to green icon
+   echo "%{A1:insync start:}%{T1}%{F#5be610}󰤉%{T-}%{A}"
 else
-    feh --bg-scale "$RED_ICON" # Set background to red icon
+   echo "%{A1:insync start:}%{T1}%{F#FF0000}󰤉%{T-}%{A}"
 fi
