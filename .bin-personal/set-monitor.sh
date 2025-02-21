e1c1182fd9f47b1d37d7b06a74252e31a5e0d0b4
Brett Crisp <brettcrisp2@gmail.com>
2025-02-21 08:52:15 +0700

    Updated script
    The script was working fine on the desktop, but on the laptop is was opening to workspace 10. The change should fix this

--- .bin-personal/set-monitor.sh
+++ .bin-personal/set-monitor.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Detect connected output and set workspaces
 OUTPUT=$(xrandr | grep " connected" | awk '{print $1}' | head -n 1)
-i3-msg "workspace 1 output $OUTPUT; workspace 2 output $OUTPUT; workspace 3 output $OUTPUT; workspace 4 output $OUTPUT; workspace 5 output $OUTPUT; workspace 6 output $OUTPUT; workspace 7 output $OUTPUT; workspace 8 output $OUTPUT; workspace 9 output $OUTPUT; workspace 10 output $OUTPUT"
+i3-msg "workspace 1 output $OUTPUT; workspace 2 output $OUTPUT; workspace 3 output $OUTPUT; workspace 4 output $OUTPUT; workspace 5 output $OUTPUT; workspace 6 output $OUTPUT; workspace 7 output $OUTPUT; workspace 8 output $OUTPUT; workspace 9 output $OUTPUT; workspace 10 output $OUTPUT; workspace 1"
