0a23fec949fa6d40b984eec3297526d147f639ef
Brett Crisp <brettcrisp2@gmail.com>
2025-03-22 15:19:24 +0700

    Removed a couple of script files

--- .bin-personal/realvnc-viewer-original.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-xhost +local:docker > /dev/null 2>&1
-docker run -d --rm \
-    -e DISPLAY=$DISPLAY \
-    -v /tmp/.X11-unix:/tmp/.X11-unix \
-    -v "$HOME/.vnc/realvnc:/root/.vnc" \
-    realvnc-viewer
